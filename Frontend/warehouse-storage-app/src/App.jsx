4e85bdf38681b8e30b407a7b3a25b81b8de6eb16
Ahmadullah Naibi <ahmadullah@hotmail.dk>
2026-03-11 14:52:02 +0100

    prototype image in container div for login page

--- Frontend/warehouse-storage-app/src/App.jsx
+++ Frontend/warehouse-storage-app/src/App.jsx
@@ -16,6 +16,10 @@ function App() {
         <button type="submit">Login</button>
       </div>
     </form>
+
+      <div className="container">
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/022/935/143/small/large-warehouse-for-storage-of-goods-racks-shelves-goods-background-generative-ai-photo.jpg" alt="Warehouse Storage" />
+      </div>
     </div>
   );
 }
